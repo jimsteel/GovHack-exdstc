2ba2edd880287b5f56afef47ed6a2f39fdd7033f
xbh <jm.mh.test@gmail.com>
2014-07-12 06:38:41 +0000

    API

--- vic-builder/www/api/suburb.php
+++ vic-builder/www/api/suburb.php
@@ -1,15 +1,6 @@
 <?PHP
 
-define("MYSQL_HOST", "localhost");
-define("MYSQL_USER", "root");
-define("MYSQL_PASS", "exdstc");
-define("MYSQL_DB", "vba");
-
-$mysqli = new mysqli(MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB);
-
-if ($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-}
+include_once("vba.php");
 
 if($stmt = $mysqli->prepare("SELECT Site_street, Site_suburb, Allotment_Area FROM vba WHERE Site_pcode = ? AND Allotment_Area != 0")) {
 
